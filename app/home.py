600993c4fc0aa915f5f42485c50c25cfcc9feff6
conankapoor <kapoor.shivam88@gmail.com>
2020-03-08 17:48:08 +0100

    sorting on time fixed

--- app/home.py
+++ app/home.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from app.models import Member, ChangeLog, Subscription, Subject
 from .utils import check_user_id
+from itertools import chain
 
 def feed(request):
     current_user = Member.objects.filter(u_id=request.user.id).first()
@@ -11,23 +12,20 @@ def feed(request):
 
     """
     user_subs = Subscription.objects.filter(member=current_user)
-    changelogs = []
+    changelog_sub, changelog_curriculum = [], []
     for sub in user_subs:
         # for feeds related to subject
         if sub.curriculum is None:
-            changelog = ChangeLog.objects.filter(subject=sub.subject)
-            changelogs.extend(changelog)
+            changelog_sub.extend(ChangeLog.objects.filter(subject=sub.subject))
         # for feeds related to curriculum 
         elif sub.subject is None:
-            changelog = ChangeLog.objects.filter(curriculum=sub.curriculum)
-            changelogs.extend(changelog)
+            changelog_curriculum.extend(ChangeLog.objects.filter(curriculum=sub.curriculum))
         else:
-            print("something really went wrong.")
+            print("No Changelog")
             changelogs =[]
     
-    # print(changelogs)
     # Only distinct feeds allowed
-    changelogs = list(set(changelogs))
+    changelogs = sorted(list(set(chain(changelog_sub,changelog_curriculum))), key = lambda instance: instance.created_on, reverse=True)
     
     context = {
         "changelogs": changelogs,
