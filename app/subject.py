1666901d23d847f32ff54e7d63a83bb1eb94dc73
conankapoor <kapoor.shivam88@gmail.com>
2020-03-11 06:10:33 +0100

    added contributor count to subject page

--- app/subject.py
+++ app/subject.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from app.models import Member, Subscription, Subject, Upvote, Curriculum
+from app.models import Member, Subscription, Subject, Upvote, Curriculum, Contributor
 
 
 def showsubject(request, sid):
@@ -10,13 +10,20 @@ def showsubject(request, sid):
     # Fetching the current user and subject for associated sid
     current_user = get_object_or_404(Member, u_id=request.user)
     subject = get_object_or_404(Subject, id=sid)
-    
+
     # 1. Fetching list of curriculums for the subject
     # 2. Fetch upvote count for all curriculums and store as a dictionary
     # 3. Sort the curriculums according to no. of upvotes
     curriculums = Curriculum.objects.filter(subject=subject)
-    upvote_count = dict((curriculum, Upvote.objects.filter(curriculum=curriculum).count()) for curriculum in curriculums)
-    sorted_curriculums = sorted(upvote_count.items(), key=lambda upvote: upvote[1], reverse=True)
+    upvote_count = dict((curriculum, Upvote.objects.filter(
+        curriculum=curriculum).count()) for curriculum in curriculums)
+    sorted_curriculums = sorted(
+        upvote_count.items(), key=lambda upvote: upvote[1], reverse=True)
+
+    # Fetching number of contributors for every curriculum
+    contributors = dict((curriculum, Contributor.objects.filter(curriculum=curriculum).count(
+    )) if Contributor.objects.filter(curriculum=curriculum) else (curriculum, 0) for curriculum in upvote_count.keys())
+    print(contributors)
 
     # Check if user is subscribed to the subject
     user_subscription = Subscription.objects.filter(
@@ -27,5 +34,6 @@ def showsubject(request, sid):
             'subject': subject,
             'sorted_curriculums': sorted_curriculums,
             'user_subscription': user_subscription.first(),
+            'contributors': contributors,
         }
         return render(request, 'subject.html', context)
